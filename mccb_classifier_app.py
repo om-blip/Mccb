5c810853b4e12ca62751f6200a3cae4857d18375
Om <80588362+om-blip@users.noreply.github.com>
2024-07-22 10:18:46 +0530

    Update mccb_classifier_app.py

--- mccb_classifier_app.py
+++ mccb_classifier_app.py
@@ -128,6 +128,8 @@ st.markdown(
     .title {color: #1f77b4; font-size: 36px; font-weight: bold;}
     .subtitle {color: #ff7f0e; font-size: 24px; font-weight: bold;}
     .highlight {font-size: 18px; font-weight: bold; color: #00000;}
+    .container {display: flex; justify-content: space-between;}
+    .box {width: 48%; padding: 10px; background-color: #ffffff; border: 1px solid #ddd; border-radius: 8px;}
     </style>
     """,
     unsafe_allow_html=True
@@ -156,19 +158,26 @@ if uploaded_files:
 
         # Extract and display maximum amplitude for on and off operations
         on_max, off_max = extract_amplitude(df, 1000, 10000, 40000, 50000)
-        
+
+        total_time_seconds_on, total_time_seconds_off = calculate_operation_times(df, 5000, 6000, 45100, 46000)
+        total_time_seconds_on = round(total_time_seconds_on, 5)
+        total_time_seconds_off = round(total_time_seconds_off, 5)
+
+        st.markdown('<div class="container">', unsafe_allow_html=True)
+
+        # ON Operation box
+        st.markdown('<div class="box">', unsafe_allow_html=True)
         st.markdown('<div class="subtitle">ON Operation:</div>', unsafe_allow_html=True)
         st.markdown("---")
         st.markdown(f'<span class="highlight">Vibration Amplitude: {on_max} mV</span>', unsafe_allow_html=True)
-        total_time_seconds_on, total_time_seconds_off = calculate_operation_times(df, 5000, 6000, 45100, 46000)
-        total_time_seconds_on = round(total_time_seconds_on, 5)
-        total_time_seconds_off = round(total_time_seconds_off, 5) 
         if not np.isnan(total_time_seconds_on):
             st.markdown(f'<span class="highlight">Vibration Operation Time: {total_time_seconds_on} s</span>', unsafe_allow_html=True)
         else:
             st.write("Probably fault in the data or faulty file that's why cannot calculate time and probably Amplitude is low, please upload a different file.")
         st.markdown('</div>', unsafe_allow_html=True)
 
+        # OFF Operation box
+        st.markdown('<div class="box">', unsafe_allow_html=True)
         st.markdown('<div class="subtitle">OFF Operation:</div>', unsafe_allow_html=True)
         st.markdown("---")
         st.markdown(f'<span class="highlight">Vibration Amplitude: {off_max} mV</span>', unsafe_allow_html=True)
@@ -176,4 +185,6 @@ if uploaded_files:
             st.markdown(f'<span class="highlight">Vibration Operation Time: {total_time_seconds_off} s</span>', unsafe_allow_html=True)
         else:
             st.write("Probably fault in the data or faulty file that's why cannot calculate time and probably Amplitude is low, please upload a different file.")
+        st.markdown('</div>', unsafe_allow_html=True)
+
         st.markdown('</div>', unsafe_allow_html=True)
